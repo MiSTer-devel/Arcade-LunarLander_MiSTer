1b0bb9ede41ac808af5e822a993cf3f799560c93
Alan Steremberg <alan.steremberg@gmail.com>
2019-12-19 11:30:50 -0800

    new sys, direct video

--- Arcade-LunarLander.sv
+++ Arcade-LunarLander.sv
@@ -1,4 +1,3 @@
-
 module emu
 (
 	//Master input clock
@@ -9,7 +8,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [44:0] HPS_BUS,
+	inout  [45:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        VGA_CLK,
@@ -24,6 +23,7 @@ module emu
 	output        VGA_HS,
 	output        VGA_VS,
 	output        VGA_DE,    // = ~(VBlank | HBlank)
+	output        VGA_F1,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        HDMI_CLK,
@@ -54,9 +54,19 @@ module emu
 
 	output [15:0] AUDIO_L,
 	output [15:0] AUDIO_R,
-	output        AUDIO_S    // 1 - signed audio samples, 0 - unsigned
+	output        AUDIO_S,   // 1 - signed audio samples, 0 - unsigned
+	
+	// Open-drain User port.
+	// 0 - D+/RX
+	// 1 - D-/TX
+	// 2..6 - USR2..USR6
+	// Set USER_OUT to 1 to read from USER_IN.
+	input   [6:0] USER_IN,
+	output  [6:0] USER_OUT
 );
 
+assign VGA_F1    = 0;
+assign USER_OUT  = '1;
 assign LED_USER  = ioctl_download;
 assign LED_DISK  = 0;
 assign LED_POWER = 0;
@@ -68,8 +78,6 @@ assign HDMI_ARY = status[1] ? 8'd9  : 8'd3;
 `include "build_id.v" 
 localparam CONF_STR = {
 	"A.LLANDER;;",
-	"F,rom;", // allow loading of alternate ROMs
-	"-;",
 	"OD,Thruster,Analog Stick,D-Pad;",
 	"-;",
 	"O1,Aspect Ratio,Original,Wide;",
@@ -120,6 +128,7 @@ wire [10:0] ps2_key;
 
 wire [15:0] joy_0, joy_1;
 wire [15:0] joy = joy_0 | joy_1;
+wire [21:0] gamma_bus;
 
 hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 (
@@ -131,6 +140,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.buttons(buttons),
 	.status(status),
 	.forced_scandoubler(forced_scandoubler),
+	.gamma_bus(gamma_bus),
 
 	.ioctl_download(ioctl_download),
 	.ioctl_wr(ioctl_wr),
